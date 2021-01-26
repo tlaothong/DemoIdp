f7ae087c58f95d916f179ae588e6f6d254ee4429
Teerachai Laothong <tlaothong@gmail.com>
2021-01-26 12:11:20 +0700

    Initial solution with 3 web projects

---
+++ TheWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
