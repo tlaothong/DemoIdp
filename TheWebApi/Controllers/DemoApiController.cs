e17879cc7ceeca4728168b88f5f5094f9c7976fd
Teerachai Laothong <tlaothong@gmail.com>
2021-01-27 08:38:31 +0700

    Get refid from webapi itself

--- TheWebApi/Controllers/DemoApiController.cs
+++ TheWebApi/Controllers/DemoApiController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace TheWebApi.Controllers
@@ -14,9 +18,26 @@ namespace TheWebApi.Controllers
     public class DemoApiController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+            var client = new HttpClient();
+            var metaDataResponse = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var response = await client.GetUserInfoAsync(new UserInfoRequest
+            {
+                Address = metaDataResponse.UserInfoEndpoint,
+                Token = accessToken
+            });
+            if (response.IsError)
+            {
+                throw new Exception("Problem while fetching data from the UserInfo endpoint", response.Exception);
+            }
+            var refClaim = response.Claims.FirstOrDefault(c => c.Type.Equals("refid"));
+            var claims = User.Claims.ToList();
+            if (refClaim != null)
+                claims.Add(refClaim);
+
+            return new JsonResult(from c in claims select new { c.Type, c.Value });
         }
     }
 }
